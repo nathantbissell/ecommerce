6fb4f001bd554415fe60239ce046dfd3cec3ccf9
nathantbissell <nathantbissell@gmail.com>
2019-02-25 13:03:30 -0500

    Updates

---
+++ precache-manifest.9a1fcaefdd9019929f0360a8b96eb7da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/ecommerce/static/media/logo.9f7e506d.svg"
+  },
+  {
+    "revision": "d4326592b3897613f8c1",
+    "url": "/ecommerce/static/js/runtime~main.d4326592.js"
+  },
+  {
+    "revision": "b82d8a614c52cc88ccff",
+    "url": "/ecommerce/static/js/main.b82d8a61.chunk.js"
+  },
+  {
+    "revision": "daee28447e2713ad98f4",
+    "url": "/ecommerce/static/js/2.daee2844.chunk.js"
+  },
+  {
+    "revision": "b82d8a614c52cc88ccff",
+    "url": "/ecommerce/static/css/main.85ed7b95.chunk.css"
+  },
+  {
+    "revision": "daee28447e2713ad98f4",
+    "url": "/ecommerce/static/css/2.fe311652.chunk.css"
+  },
+  {
+    "revision": "66c1ad1997c5155730308da6e1c5fe0e",
+    "url": "/ecommerce/index.html"
+  }
+];
