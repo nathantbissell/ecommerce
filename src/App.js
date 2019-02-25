73efefbcda2ace1cfb711f122861b960df35c4c1
nathantbissell <nathantbissell@gmail.com>
2019-02-25 11:08:53 -0500

    recreated files

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Switch, Route } from 'react-router-dom'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './components/Navbar'
+import Default from './components/Default'
+import ProductList from './components/ProductList'
+import Details from './components/Details'
+import Cart from './components/Cart'
+import Modal from './components/Modal'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <React.Fragment>
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={ProductList} />
+          <Route exact path='/details' component={Details} />
+          <Route exact path='/cart' component={Cart} />
+          <Route exact component={Default} />
+        </Switch>
+        <Modal />
+      </React.Fragment>
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import Title from '../Title'
+import CartColumns from './CartColumns'
+import EmptyCart from './EmptyCart'
+import CartList from './CartList'
+import CartTotals from './CartTotals'
+import { ProductConsumer } from '../../context'
+export default class Cart extends Component {
+  render() {
+    return (
+      <section>
+        <ProductConsumer>
+          {value => {
+            const { cart } = value
+            if (cart.length > 0) {
+              return (
+                <React.Fragment>
+                  <Title name='My' title='cart' />
+                  <CartColumns />
+                  <CartList value={value} />
+                  <CartTotals value={value} />
+                </React.Fragment>
+              )
+            } else {
+              return <EmptyCart />
+            }
+          }}
+        </ProductConsumer>
+      </section>
+    )
+  }
+}
