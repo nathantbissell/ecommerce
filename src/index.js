3b678809162bf69543101a7091169db383dfabe6
nathantbissell <nathantbissell@gmail.com>
2019-02-25 22:24:42 -0500

    changed browserrouter to hashrouter

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { ProductProvider } from './context'
 
 ReactDOM.render(
   <ProductProvider>
-    <Router>
+    <HashRouter>
       <App />
-    </Router>
+    </HashRouter>
   </ProductProvider>,
   document.getElementById('root')
 )
@@ -18,4 +18,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister()
+// serviceWorker.unregister()
