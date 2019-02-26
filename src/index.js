1272cf539ede075c4634984cf3d6940c6f0b0724
nathantbissell <nathantbissell@gmail.com>
2019-02-25 22:29:42 -0500

    removed all browserRouter code

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import * as serviceWorker from './serviceWorker'
 import { HashRouter } from 'react-router-dom'
 import { ProductProvider } from './context'
 
